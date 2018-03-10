b3d902f11b185f8502a3031b60cbf9f426096caa
Harold Schreckengost <harold@haroldmschreckengost.com>
2018-03-10 11:47:16 -0600

    Added a few new aliases (gmd and gmm)

--- sourceme.sh
+++ sourceme.sh
@@ -1,5 +1,5 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$HOME/.scripts"
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	export PATH=$DIR/$line:$PATH
-done < packages
+done < $DIR/packages
